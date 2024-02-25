eb0481c2ba08fc68b5e041cba4d5cef2aa984b04
Chakib Bachir <chakibbachir88@gmail.com>
2024-02-25 22:08:28 +0100

    ok

--- push_swap_NEW/src/push_swap/sort_stacks.c
+++ push_swap_NEW/src/push_swap/sort_stacks.c
@@ -6,7 +6,7 @@
 /*   By: chbachir <chbachir@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 14:07:54 by chbachir          #+#    #+#             */
-/*   Updated: 2024/02/23 14:52:38 by chbachir         ###   ########.fr       */
+/*   Updated: 2024/02/25 14:31:20 by chbachir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,26 @@
 void	set_index(t_stack *stack)
 {
 	int	i;
+
 	if (!stack)
-		return;
+		return ;
 	i = 0;
 	while (stack)
 	{
-        stack->index = i;
-        ft_printf("i %d = n %d\n", stack->index, stack->nbr);
-        stack = stack->next;
+		stack->index = i;
+		ft_printf("i %d = n %d\n", stack->index, stack->nbr);
+		stack = stack->next;
 		i++;
 	}
 }
 
 void	sort_stacks(t_stack **a, t_stack **b)
 {
-	b = NULL; //!
 	int			len_a;
 	int			mid_stack;
 	t_stack		*min_node;
-	
+
+	//b = NULL;
 	len_a = stack_len(*a);
 	mid_stack = len_a / 2;
 	min_node = get_min(*a);
